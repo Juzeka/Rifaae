68e5afb0984281fc05a90d5d27655975aacfc9be
juzeka <rafaelgomesalmeida@hotmail.com>
2022-05-06 00:37:05 -0300

    alterando nome variavel

--- bilhetes/services.py
+++ bilhetes/services.py
@@ -7,8 +7,8 @@ class BilheteService:
     def __init__(self, **kwargs):
         ...
 
-    def gerar_numero(self, initial, stop):
-        return random.randint(initial, stop)
+    def gerar_numero(self, init, stop):
+        return random.randint(init, stop)
 
     def gerar_parte_numerica(self):
         first = self.gerar_numero(1, 9)
