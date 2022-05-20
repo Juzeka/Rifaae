55fcf5b081cf66a4f90adcb0e4ba0d92d8e662af
juzeka <rafaelgomesalmeida@hotmail.com>
2022-05-20 20:58:32 -0300

    criando view list de bilhetes

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('clientes/', include('clientes.urls'), name='clientes'),
+    path('cliente/bilhetes/', include('bilhetes.urls'), name='cliente_bilhetes'),
     path('organizadores/', include('organizadores.urls'), name='organizadores'),
     path('pessoas/', include('pessoas.urls'), name='pessoas'),
     path('logradouros/', include('logradouros.urls'), name='logradouros'),
-    path('bilhetes/', include('bilhetes.urls'), name='bilhetes'),
 ]
